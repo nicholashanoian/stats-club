28d2f866722e05096ff553c5269452a3c2883e1d
Nicholas Hanoian <nicholashanoian@gmail.com>
2019-03-05 14:49:23 -0500

    made secret key secure

--- website/settings.py
+++ website/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#qf$mu%few5440iza%lq419rakz53my4*j5n3ux(1)kbp8cks$'
+with open('../secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
